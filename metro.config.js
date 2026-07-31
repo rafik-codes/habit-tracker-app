bf72d9bac7fb08b41fed9698bf9ceda47a9b6ded
rafik-codes <rafik.tech.dev@gmail.com>
2026-07-31 02:58:45 +0000

    simplify metro config to default

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,7 @@
+cat > metro.config.js << 'EOF'
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-// قصر المراقبة على المجلد الحالي فقط
-config.watchFolders = [__dirname];
-
-// التأكد من أن المسارات لا تحاول الوصول للمجلد الرئيسي
-config.resolver.nodeModulesPaths = [path.resolve(__dirname, "node_modules")];
-
 module.exports = config;
+EOF
